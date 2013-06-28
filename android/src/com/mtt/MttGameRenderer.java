7ab4c309645423ee4873d42a94a2d7a9f54822ac
Johannes H. Strom <jstrom@gmail.com>
2013-06-28 00:12:30 -0400

    Resolved ES column major limitation, rendering mtt board

--- android/src/com/mtt/MttGameRenderer.java
+++ android/src/com/mtt/MttGameRenderer.java
@@ -1,14 +1,20 @@
 package com.mtt;
 
+import java.util.*;
 import java.nio.*;
+
+import android.opengl.Matrix;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.*;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.util.Log;
 
 public class MttGameRenderer implements GLSurfaceView.Renderer
 {
+    private static final String TAG = "MyGLRenderer";
+
     private final String vertexShaderCode =
         "attribute vec4 vPosition;" +
         "void main() {" +
@@ -35,13 +41,158 @@ public class MttGameRenderer implements GLSurfaceView.Renderer
         return shader;
     }
 
+    /**
+     * Utility method for debugging OpenGL calls. Provide the name of the call
+     * just after making it:
+     *
+     * <pre>
+     * mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+     * MyGLRenderer.checkGlError("glGetUniformLocation");</pre>
+     *
+     * If the operation is not successful, the check throws an error.
+     *
+     * @param glOperation - Name of the OpenGL call to check.
+     */
+    public static void checkGlError(String glOperation) {
+        int error;
+        while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+            Log.e(TAG, glOperation + ": glError " + error);
+            throw new RuntimeException(glOperation + ": glError " + error);
+        }
+    }
+
     MttGame game;
+
+    ArrayList<BoardView> boards = new ArrayList();
 
     public MttGameRenderer(MttGame game)
     {
         this.game = game;
+
     }
 
+
+    class BoardView
+    {
+        private final String singleColorVert =
+            "uniform mat4 PM;"+
+            "attribute vec4 position;" +
+            "void main() {" +
+            "  gl_Position =  position * PM;"+ // Post multiply because opengl es only allows column major matrices
+            "}";
+
+        private final String singleColorFrag =
+            "precision mediump float;" +
+            "uniform vec4 color;" +
+            "void main() {" +
+            "  gl_FragColor = color;" +
+            "}";
+
+        float pts[] = {0,1,3,1,
+                       0,2,3,2,
+                       1,0,1,3,
+                       2,0,2,3};
+
+
+        private final float PM[] = {1,0,0,0,
+                                    0,1,0,0,
+                                    0,0,1,0,
+                                    0,0,0,1};
+
+        float color[];// = {1.0f,1.0f,1.0f,1.0f};
+        float linewd = 10.0f;
+
+        int programID;
+        FloatBuffer vertexBuffer;
+
+        FloatBuffer fbPM;
+
+        public void print(float v[])
+        {
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    System.out.printf("%14f ", v[i*4 + j]);
+                }
+                System.out.printf("\n");
+            }
+        }
+
+        public BoardView(float offX, float offY, float scale, float linewd, float[]  color)
+        {
+            this.color = color;
+            this.linewd = linewd;
+
+            PM[0*4 + 3] = offX;
+            PM[1*4 + 3] = offY;
+            PM[0*4 + 0] = PM[1*4 + 1] = scale;
+
+            int sizeoffloat = 4;
+            {
+                ByteBuffer bb = ByteBuffer.allocateDirect(pts.length*sizeoffloat);
+                bb.order(ByteOrder.nativeOrder());
+
+                vertexBuffer = bb.asFloatBuffer();
+
+                vertexBuffer.put(pts);
+                vertexBuffer.position(0);
+            }
+
+            int vertID = loadShader(GLES20.GL_VERTEX_SHADER, singleColorVert);
+            int fragID = loadShader(GLES20.GL_FRAGMENT_SHADER, singleColorFrag);
+            programID = GLES20.glCreateProgram();
+            System.out.println("Program ID board "+ programID);
+            GLES20.glAttachShader(programID, vertID);
+            GLES20.glAttachShader(programID, fragID);
+            GLES20.glLinkProgram(programID);
+
+
+            {
+                ByteBuffer bb = ByteBuffer.allocateDirect(PM.length*sizeoffloat);
+                bb.order(ByteOrder.nativeOrder());
+
+                fbPM = bb.asFloatBuffer();
+
+                fbPM.put(PM);
+                fbPM.position(0);
+            }
+        }
+
+        void draw()
+        {
+            System.out.println("Program ID board "+ programID);
+            GLES20.glUseProgram(programID);
+
+            int vertexCount = pts.length/2;
+            int vertexStride = 2 * 4; // bytes per vertex
+
+            int posAttr = GLES20.glGetAttribLocation(programID, "position");
+            System.out.println("posAttr "+ posAttr);
+
+            GLES20.glEnableVertexAttribArray(posAttr);
+            GLES20.glVertexAttribPointer(posAttr, 2, GLES20.GL_FLOAT, false, vertexStride, vertexBuffer);
+
+
+
+            int pmUnif = GLES20.glGetUniformLocation(programID, "PM");
+            GLES20.glUniformMatrix4fv(pmUnif, 1, false, PM, 0);
+            System.out.println("pmUnif "+ pmUnif);
+            //checkGlError("glUniformMatrix4fv");
+
+            int colorUnif = GLES20.glGetUniformLocation(programID, "color");
+            System.out.println("colorUnif "+ colorUnif);
+            System.out.printf("[%.3f,%.3f,%.3f,%.3f]\n", color[0],color[1],color[2],color[3]);
+            GLES20.glUniform4fv(colorUnif, 1, color, 0);
+
+            GLES20.glLineWidth(linewd);
+
+            System.out.println("vertexCount "+ vertexCount);
+            GLES20.glDrawArrays(GLES20.GL_LINES, 0, vertexCount);
+
+            GLES20.glDisableVertexAttribArray(posAttr);
+        }
+
+
+    }
 
 
     void drawTriangle()
@@ -85,7 +236,7 @@ public class MttGameRenderer implements GLSurfaceView.Renderer
         int colorUnif = GLES20.glGetUniformLocation(programID, "vColor");
         GLES20.glUniform4fv(colorUnif, 1, color, 0);
 
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
+        GLES20.glDrawArrays(GLES20.GL_LINE_LOOP, 0, vertexCount);
 
         GLES20.glDisableVertexAttribArray(posAttr);
 
@@ -97,6 +248,17 @@ public class MttGameRenderer implements GLSurfaceView.Renderer
         // Set the background frame color
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
+
+        boards.add(new BoardView(-1,-1,.66666f, 10, new float[]{.6f,.6f,.6f,1.0f}));
+
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++) {
+                float pad = .8f;
+                float scale  = (.666f / 3) * pad;
+                float offX = -1 + i * .6666f  + .1f*.6666f;
+                float offY = -1 + j * .6666f  + .1f*.6666f;
+                boards.add(new BoardView(offX, offY, scale, 3, new float[]{1.0f,1.0f,1.0f,1.0f}));
+            }
     }
 
     @Override
@@ -105,10 +267,11 @@ public class MttGameRenderer implements GLSurfaceView.Renderer
         // Draw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 
+        for (BoardView bv : boards)
+            bv.draw();
 
-        drawTriangle();
 
-        System.out.println("onDrawFrame()");
+        System.out.println("onDrawFrame()" + boards.size());
 
     }
 
