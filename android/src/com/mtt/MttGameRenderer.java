70be9663249726f9e316a5df06476549b4b28d36
Johannes H. Strom <jstrom@gmail.com>
2013-07-01 19:47:36 -0400

    Textures are loading and displaying without crashing, but they don't look as expected

--- android/src/com/mtt/MttGameRenderer.java
+++ android/src/com/mtt/MttGameRenderer.java
@@ -15,18 +15,8 @@ public class MttGameRenderer implements GLSurfaceView.Renderer
 {
     private static final String TAG = "MyGLRenderer";
 
-    private final String vertexShaderCode =
-        "attribute vec4 vPosition;" +
-        "void main() {" +
-        "  gl_Position = vPosition;" +
-        "}";
-
-    private final String fragmentShaderCode =
-        "precision mediump float;" +
-        "uniform vec4 vColor;" +
-        "void main() {" +
-        "  gl_FragColor = vColor;" +
-        "}";
+    static final int sizeoffloat = 4;
+    static final int sizeofshort = 2;
 
     public static int loadShader(int type, String shaderCode){
 
@@ -37,6 +27,18 @@ public class MttGameRenderer implements GLSurfaceView.Renderer
         // add the source code to the shader and compile it
         GLES20.glShaderSource(shader, shaderCode);
         GLES20.glCompileShader(shader);
+
+        // Get the link status.
+        final int[] linkStatus = new int[1];
+        GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, linkStatus, 0);
+
+        String error = GLES20.glGetShaderInfoLog(shader);
+        // If the link failed, delete the program.
+        // if (error.size() != 0)
+        if (linkStatus[0] != GLES20.GL_TRUE)
+        {
+            Log.e(TAG, "Error compiling shader: " + error);
+        }
 
         return shader;
     }
@@ -69,6 +71,32 @@ public class MttGameRenderer implements GLSurfaceView.Renderer
     {
         this.game = game;
 
+    }
+
+
+
+    static ByteBuffer wrapBuffer(float data[])
+    {
+        ByteBuffer bb = ByteBuffer.allocateDirect(data.length*sizeoffloat);
+        bb.order(ByteOrder.nativeOrder());
+
+        FloatBuffer fb = bb.asFloatBuffer();
+
+        fb.put(data);
+        fb.position(0);
+        return bb;
+    }
+
+    static ByteBuffer wrapBuffer(short data[])
+    {
+        ByteBuffer bb = ByteBuffer.allocateDirect(data.length*sizeofshort);
+        bb.order(ByteOrder.nativeOrder());
+
+        ShortBuffer fb = bb.asShortBuffer();
+
+        fb.put(data);
+        fb.position(0);
+        return bb;
     }
 
 
@@ -111,17 +139,19 @@ public class MttGameRenderer implements GLSurfaceView.Renderer
             "}";
 
         private final String texVert =
+            "precision mediump float;"+
             "uniform mat4 PM;"+
             "attribute vec4 position;"+
             "attribute vec2 texIn;"+
             "varying vec2 texOut;"+
             "void main()"+
             "{"+
-            "    gl_Position = PM*position;"+
+            "    gl_Position = position*PM;"+ // post mult
             "    texOut = texIn;"+
             "}";
 
         private final String texFrag =
+            "precision mediump float;"+
             "uniform sampler2D texture;"+
             "varying vec2 texOut;"+
             "void main()"+
@@ -158,8 +188,6 @@ public class MttGameRenderer implements GLSurfaceView.Renderer
                 System.out.printf("\n");
             }
         }
-
-        final int sizeoffloat = 4;
 
         public BoardView(float offX, float offY, float scale, float linewd, float[]  color)
         {
@@ -202,11 +230,23 @@ public class MttGameRenderer implements GLSurfaceView.Renderer
             byte rawbytes[] = values.getBytes(); // no trailing \0
             ByteBuffer bb = ByteBuffer.allocateDirect(rawbytes.length);
 
+            checkGlError("pre Make tex");
+
+            //GLES20.glEnable(GLES20.GL_TEXTURE_2D);
+            //checkGlError("glEnable");
             int texid_arr[] = {0};
             GLES20.glGenTextures(1, texid_arr, 0);
             int texid = texid_arr[0];
+            checkGlError("glGenTextures init");
+
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, texid);
+            checkGlError("glBindTexture init");
+            // Interpolate textures
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
 
             GLES20.glTexImage2D(GLES20.GL_TEXTURE_2D, 0, format, width, height, 0, format, GLES20.GL_UNSIGNED_BYTE, bb);
+            checkGlError("glTexImage2D init");
 
             return texid;
         }
@@ -221,6 +261,17 @@ public class MttGameRenderer implements GLSurfaceView.Renderer
             GLES20.glAttachShader(programID, vertID);
             GLES20.glAttachShader(programID, fragID);
             GLES20.glLinkProgram(programID);
+            //checkGlError("glLinkProgram");
+
+            // Get the link status.
+			final int[] linkStatus = new int[1];
+			GLES20.glGetProgramiv(programID, GLES20.GL_LINK_STATUS, linkStatus, 0);
+
+			// If the link failed, delete the program.
+			if (linkStatus[0] == 0)
+			{
+				Log.e(TAG, "Error compiling program: " + GLES20.glGetProgramInfoLog(programID));
+            }
             return programID;
         }
 
@@ -232,7 +283,64 @@ public class MttGameRenderer implements GLSurfaceView.Renderer
 
         void drawXOs()
         {
+            drawTexture(texProgramID, xTexID, 0, 0);
+        }
 
+        // Draw a texture inside the ith,jth box on the board
+        void drawTexture(int programID, int texID, int rowIdx, int colIdx)
+        {
+
+            GLES20.glUseProgram(programID);
+
+            int attrTexI = GLES20.glGetUniformLocation(programID, "texture");
+            GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+            checkGlError("activeTexture draw");
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, texID);
+            checkGlError("bindTexture");
+            GLES20.glUniform1i(attrTexI, 0); // Bind the uniform to TEXTURE0
+            checkGlError("textureUnif");
+
+            int pmUnif = GLES20.glGetUniformLocation(programID, "PM");
+            GLES20.glUniformMatrix4fv(pmUnif, 1, false, PM, 0);
+            System.out.println("tex PM "+ pmUnif);
+
+            ByteBuffer fbTri = wrapBuffer(
+                new short[]{0,1,2, 2,3,0});
+
+            ByteBuffer fbPos = wrapBuffer(
+                new float[]{rowIdx + 0.0f, colIdx + 0.0f,
+                            rowIdx + 0.0f, colIdx + 1.0f,
+                            rowIdx + 1.0f, colIdx + 1.0f,
+                            rowIdx + 1.0f, colIdx + 0.0f});
+
+            ByteBuffer fbTex = wrapBuffer(
+                new float[] {0.0f,0.0f,
+                             0.0f,1.0f,
+                             1.0f,1.0f,
+                             1.0f,0.0f}); // account for y flip?
+
+
+
+            int posAttr = GLES20.glGetAttribLocation(programID, "position");
+            GLES20.glEnableVertexAttribArray(posAttr);
+            GLES20.glVertexAttribPointer(posAttr, 2, GLES20.GL_FLOAT, false, 2*sizeoffloat, fbPos);
+            System.out.println("tex Pos "+ posAttr);
+            checkGlError("tex:posAttr");
+
+            int texAttr = GLES20.glGetAttribLocation(programID, "texIn");
+            GLES20.glEnableVertexAttribArray(texAttr);
+            GLES20.glVertexAttribPointer(texAttr, 2, GLES20.GL_FLOAT, false, 2*sizeoffloat, fbTex);
+            System.out.println("tex Tex "+ texAttr);
+            checkGlError("tex:texAttr");
+
+
+            GLES20.glDrawElements(GLES20.GL_TRIANGLES, 6, GLES20.GL_UNSIGNED_SHORT, fbTri);
+            checkGlError("tex:drawElements");
+
+            GLES20.glDisableVertexAttribArray(posAttr);
+            GLES20.glDisableVertexAttribArray(texAttr);
+            checkGlError("tex:texAttr");
+            checkGlError("tex:posAttr");
         }
 
         void drawBoard()
@@ -243,12 +351,9 @@ public class MttGameRenderer implements GLSurfaceView.Renderer
             int vertexStride = 2 * 4; // bytes per vertex
 
             int posAttr = GLES20.glGetAttribLocation(linesProgramID, "position");
-            System.out.println("posAttr "+ posAttr);
 
             GLES20.glEnableVertexAttribArray(posAttr);
             GLES20.glVertexAttribPointer(posAttr, 2, GLES20.GL_FLOAT, false, vertexStride, vertexBuffer);
-
-
 
             int pmUnif = GLES20.glGetUniformLocation(linesProgramID, "PM");
             GLES20.glUniformMatrix4fv(pmUnif, 1, false, PM, 0);
@@ -259,60 +364,11 @@ public class MttGameRenderer implements GLSurfaceView.Renderer
 
             GLES20.glLineWidth(linewd);
 
-            System.out.println("vertexCount "+ vertexCount);
             GLES20.glDrawArrays(GLES20.GL_LINES, 0, vertexCount);
 
             GLES20.glDisableVertexAttribArray(posAttr);
         }
 
-
-    }
-
-
-    void drawTriangle()
-    {
-        ByteBuffer bb = ByteBuffer.allocateDirect(3*3*4);
-        bb.order(ByteOrder.nativeOrder());
-
-        FloatBuffer vertexBuffer = bb.asFloatBuffer();
-
-        // number of coordinates per vertex in this array
-        int COORDS_PER_VERTEX = 3;
-        float triangleCoords[] = { // in counterclockwise order:
-            0.0f,  0.622008459f, 0.0f,   // top
-            -0.5f, -0.311004243f, 0.0f,   // bottom left
-            0.5f, -0.311004243f, 0.0f    // bottom right
-        };
-
-        // Set color with red, green, blue and alpha (opacity) values
-        float color[] = { 0.5f, 0.3f, 0.1f, 1.0f };
-
-        vertexBuffer.put(triangleCoords);
-        vertexBuffer.position(0);
-
-        int vertID = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
-        int fragID = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
-        int programID = GLES20.glCreateProgram();
-        GLES20.glAttachShader(programID, vertID);
-        GLES20.glAttachShader(programID, fragID);
-        GLES20.glLinkProgram(programID);
-
-
-        GLES20.glUseProgram(programID);
-
-        int vertexCount = triangleCoords.length / COORDS_PER_VERTEX;
-        int vertexStride = COORDS_PER_VERTEX * 4; // bytes per vertex
-
-        int posAttr = GLES20.glGetAttribLocation(programID, "vPosition");
-        GLES20.glEnableVertexAttribArray(posAttr);
-        GLES20.glVertexAttribPointer(posAttr, 3, GLES20.GL_FLOAT, false, vertexStride, vertexBuffer);
-
-        int colorUnif = GLES20.glGetUniformLocation(programID, "vColor");
-        GLES20.glUniform4fv(colorUnif, 1, color, 0);
-
-        GLES20.glDrawArrays(GLES20.GL_LINE_LOOP, 0, vertexCount);
-
-        GLES20.glDisableVertexAttribArray(posAttr);
 
     }
 
